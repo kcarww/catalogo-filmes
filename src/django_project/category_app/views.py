7475a4eb7f60eda21edac67f70cff9e9304b586d
kcarww <carlos.oe.cc@gmail.com>
2024-10-13 16:00:17 -0300

    [F2416] feat: created get route, added rest_framework and tested

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+class CategoryViewSet(viewsets.ViewSet):
+    def list(self, request: Request) -> Response:
+        return Response(status=HTTP_200_OK,
+                        data=[
+                            {
+                                "id": "a5b518c3-a34a-459a-b33f-c986e1f62114",
+                                "name": "Movie",
+                                "description": "Movies category",
+                                "is_active": True
+                            },
+                            {
+                                "id": "18836c68-c71d-4346-b545-37f6205f09c9",
+                                "name": "Movie 2",
+                                "description": "Movies category",
+                                "is_active": False
+                            }   
+                        ]
+                        )
