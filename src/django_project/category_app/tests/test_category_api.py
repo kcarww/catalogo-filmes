fa3da3209dfb080f520edfb2606f56c3020f674b
kcarww <carlos.oe.cc@gmail.com>
2024-10-14 13:19:42 -0300

    [F2424] feat: create category serializer and post category

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,3 +1,4 @@
+import uuid
 import pytest
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
@@ -38,14 +39,16 @@ class TestCategoryAPI(APITestCase):
 
         url = '/api/categories/'
         response = self.client.get(url)
-        expected_data = [
-            {
-                "id": str(category_movie.id),
-                "name": category_movie.name,
-                "description": category_movie.description,
-                "is_active": category_movie.is_active
-            }
-        ]
+        expected_data = {
+            "data": [
+                {
+                    "id": str(category_movie.id),
+                    "name": category_movie.name,
+                    "description": category_movie.description,
+                    "is_active": category_movie.is_active
+                }
+            ]
+        }
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, expected_data)
@@ -57,7 +60,7 @@ class TestRetrieveAPI:
         url = "/api/categories/invalid_id/"
         response = APIClient().get(url)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-    
+
     def test_return_category_when_exists(
         self,
         category_movie: Category,
@@ -68,20 +71,58 @@ class TestRetrieveAPI:
         category_repository.save(category_documentary)
         url = f"/api/categories/{category_documentary.id}/"
         response = APIClient().get(url)
-        
+
         expected_data = {
-            "id": str(category_documentary.id),
-            "name": category_documentary.name,
-            "description": category_documentary.description,
-            "is_active": category_documentary.is_active
+            "data": {
+                "id": str(category_documentary.id),
+                "name": category_documentary.name,
+                "description": category_documentary.description,
+                "is_active": category_documentary.is_active
+            }
         }
-        
+
         assert response.status_code == status.HTTP_200_OK
         assert response.data == expected_data
-        
-        
+
     def test_if_category_does_not_exist(self):
         url = "/api/categories/18836c68-c71d-4346-b545-37f6205f09c9/"
         response = APIClient().get(url)
         assert response.status_code == status.HTTP_404_NOT_FOUND
-        
+
+
+@pytest.mark.django_db
+class TestCreateCategoryAPI:
+    def test_when_payload_is_invalid_then_return_400(self):
+        url = "/api/categories/"
+        response = APIClient().post(
+            url,
+            data={
+                "name": "",
+                "description": "Movies category",
+                "is_active": True
+            }
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_when_payload_is_valid_then_create_category_and_return_201(
+        self,
+        category_repository: DjangoORMCategoryRepository
+    ):
+        url = "/api/categories/"
+        response = APIClient().post(
+            url,
+            data = {
+                "name": "Movie",
+                "description": "Movies category",
+                "is_active": True
+            }
+        )
+        created_category_id = uuid.UUID(response.data["id"])
+        assert response.status_code == status.HTTP_201_CREATED
+        assert category_repository.get_by_id(created_category_id) == Category(
+            id=created_category_id,
+            name="Movie",
+            description="Movies category",
+            is_active=True
+        )
--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,14 +1,15 @@
-from uuid import UUID
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 
+from core.category.application.use_cases.create_category import CreateCategory, CreateCategoryRequest, CreateCategoryResponse
 from core.category.application.use_cases.exceptions import CategoryNotFound
 from core.category.application.use_cases.get_category import GetCategory, GetCategoryRequest
 from core.category.application.use_cases.list_category import ListCategory, ListCategoryRequest
 from django_project.category_app.repository import DjangoORMCategoryRepository
+from django_project.category_app.serializers import  CreateCategoryRequestSerializer, CreateCategoryResponseSerializer, ListCategoryResponseSerializer, RetrieveCategoryRequestSerializer, RetrieveCategoryResponseSerializer
 
 
 class CategoryViewSet(viewsets.ViewSet):
@@ -17,41 +18,40 @@ class CategoryViewSet(viewsets.ViewSet):
         use_case = ListCategory(repository=DjangoORMCategoryRepository())
         output = use_case.execute(input)
 
-        categories = [
-            {
-                "id": str(category.id),
-                "name": category.name,
-                "description": category.description,
-                "is_active": category.is_active
-            }
-            for category in output.data
-        ]
+        serializer = ListCategoryResponseSerializer(instance=output)
 
         return Response(status=status.HTTP_200_OK,
-                        data=categories
+                        data=serializer.data
                         )
 
     def retrieve(self, request: Request, pk=None):
-        try:
-            category_pk = UUID(pk)
-        except ValueError:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = RetrieveCategoryRequestSerializer(data={"id": pk})
+        serializer.is_valid(raise_exception=True)
 
         use_case = GetCategory(repository=DjangoORMCategoryRepository())
-        
-        try: 
-            result = use_case.execute(request=GetCategoryRequest(id=category_pk))
+
+        try:
+            result = use_case.execute(request=GetCategoryRequest(
+                id=serializer.validated_data["id"]))
         except CategoryNotFound:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        category_output = {
-            "id": str(result.id),
-            "name": result.name,
-            "description": result.description,
-            "is_active": result.is_active
-        }
+        category_output = RetrieveCategoryResponseSerializer(instance=result)
 
         return Response(
             status=status.HTTP_200_OK,
-            data=category_output
+            data=category_output.data
+        )
+        
+    def create(self, request: Request) -> Response:
+        serializer = CreateCategoryRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        category_input = CreateCategoryRequest(**serializer.validated_data)
+        use_case = CreateCategory(repository=DjangoORMCategoryRepository())
+        category_output = use_case.execute(request=category_input)
+        
+        return Response(
+            status=status.HTTP_201_CREATED,
+            data=CreateCategoryResponseSerializer(instance=category_output).data
         )
