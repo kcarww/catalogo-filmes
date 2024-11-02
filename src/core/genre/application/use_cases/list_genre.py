1215626ee6a29875c20fc92ddaf7e59baadc0f4d
kcarww <carlos.oe.cc@gmail.com>
2024-11-02 16:41:00 -0300

    feat: list genres and integrations tests for list genres

---
+++ src/core/genre/application/use_cases/list_genre.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from operator import is_
+from uuid import UUID
+from core.genre.domain.genre import Genre
+from core.genre.domain.genre_repository import GenreRepository
+
+@dataclass
+class GenreOutput:
+    id: UUID
+    name: str
+    categories: set[UUID]
+    is_active: bool
+    
+class ListGenre:
+    def __init__(self, repository: GenreRepository):
+        self.repository = repository
+        
+    @dataclass
+    class Input:
+        pass
+    
+    @dataclass
+    class Output:
+        data: list[GenreOutput]
+        
+    def execute(self, input: Input) -> Output:
+        genres = self.repository.list()
+        
+        data = [
+            GenreOutput(
+                id=genre.id,
+                name=genre.name,
+                categories=genre.categories,
+                is_active=genre.is_active
+            )
+            for genre in genres
+        ]
+        
+        return self.Output(data=data)
