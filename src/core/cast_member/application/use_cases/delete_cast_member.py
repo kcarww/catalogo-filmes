072f3bf751b89942a006698e110b5cc840983776
kcarww <carlos.oe.cc@gmail.com>
2024-11-09 09:13:12 -0300

    feat: cast member use case

---
+++ src/core/cast_member/application/use_cases/delete_cast_member.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+from core.cast_member.application.use_cases.exceptions import CastMemberNotFound
+from core.cast_member.domain.cast_member_repository import CastMemberRepository
+
+
+@dataclass
+class DeleteCastMemberInput:
+    id: UUID
+    
+class DeleteCastMember:
+    def __init__(self, repository: CastMemberRepository):
+        self.repository = repository
+        
+    def execute(self, request: DeleteCastMemberInput):
+        cast_member = self.repository.get_by_id(input.id)
+
+        if cast_member is None:
+            raise CastMemberNotFound(f"CastMember with {request.id} not found")
+        
+        self.repository.delete(cast_member.id)
+        
