892547671e7e59e6f5bab5a4256e8a99bae02b16
kcarww <carlos.oe.cc@gmail.com>
2024-11-23 09:49:53 -0300

    feat: created video orm repository

--- src/django_project/video_app/repository.py
+++ src/django_project/video_app/repository.py
@@ -1,10 +1,11 @@
+from base64 import encode
 from uuid import UUID
 from django.db import transaction
 
 from core.video.domain.value_objects import AudioVideoMedia
 from core.video.domain.video import Video
 from core.video.domain.video_repository import VideoRepository
-from django_project.video_app.models import Video as VideoModel
+from django_project.video_app.models import Video as VideoModel, AudioVideoMedia as AudioVideoMediaModel
 
 class DjangoORMVideoRepository(VideoRepository):
     def save(self, video: Video) -> None:
@@ -21,7 +22,50 @@ class DjangoORMVideoRepository(VideoRepository):
             video_model.categories.set(video.categories)
             video_model.genres.set(video.genres)
             video_model.cast_members.set(video.cast_members)
+            
+    
+    def get_by_id(self, id: UUID) -> Video:
+        try:
+            model = VideoModel.objects.get(id=id)
+        except VideoModel.DoesNotExist:
+            return None
+        else:
+            return VideoModelMapper.to_entity(model)
+        
+    def delete(self, id: UUID) -> None:
+        VideoModel.objects.filter(id=id).delete()
+        
+    def list(self) -> list[Video]:
+        return [VideoModelMapper.to_entity(model) for model in VideoModel.objects.all()]
+    
+    def update(self, video: Video) -> None:
+        with transaction.atomic():
+            try:
+                video_model = VideoModel.objects.get(pk=video.id)
+            except VideoModel.DoesNotExist:
+                return None
+            else:
+                with transaction.atomic():
+                    AudioVideoMediaModel.objects.filter(id=video_model.video.id).delete() # type: ignore
+                    video_model.video = AudioVideoMediaModel( # type: ignore
+                        name=video.video.name,
+                        raw_location=video.video.raw_location,
+                        encoded_location=video.video.encoded_location,
+                        status=video.video.status,
+                    ) if video.video else None
+                    
+                    video_model.categories.set(video.categories)
+                    video_model.genres.set(video.genres)
+                    video_model.cast_members.set(video.cast_members)
+                    
+                    video_model.title = video.title
+                    video_model.description = video.description
+                    video_model.launch_year = video.launch_year
+                    video_model.duration = video.duration
+                    video_model.rating = video.rating
+                    video_model.published = video.published
 
+                    video_model.save()
 
 class VideoModelMapper:
     @staticmethod
