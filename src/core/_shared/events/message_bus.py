491fe3a93a5887907428345c9bdf8e80f332b15b
kcarww <carlos.oe.cc@gmail.com>
2024-12-03 19:04:00 -0300

    feat: teste message bus

--- src/core/_shared/events/message_bus.py
+++ src/core/_shared/events/message_bus.py
@@ -1,7 +1,14 @@
+from typing import Type
+from core._shared.application.handler import Handler
 from core._shared.events.abstract_message_bus import AbstractMessageBus
 from core._shared.events.event import Event
 
 class MessageBus(AbstractMessageBus):
+    def __init__(self) -> None:
+        self.handlers: dict[Type[Event], list[Handler]] = {}
+    
     def handle(self, events: list[Event]) -> None:
-        print("Handling events", events)
+        for event in events:
+            for handler in self.handlers[type(event)]:
+                handler.handle(event)
         
