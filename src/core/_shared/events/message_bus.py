44863f9853f88143fe564c7282e1f312872641fc
kcarww <carlos.oe.cc@gmail.com>
2024-12-05 18:55:16 -0300

    feat: added handler value for handlers property

--- src/core/_shared/events/message_bus.py
+++ src/core/_shared/events/message_bus.py
@@ -1,11 +1,25 @@
+import logging
 from typing import Type
 from core._shared.application.handler import Handler
 from core._shared.events.abstract_message_bus import AbstractMessageBus
 from core._shared.events.event import Event
+from core._shared.infrastructure.events.rabbitmq_dispatcher import RabbitMQDispatcher
+from core.video.application.events.handlers import DummyHandler, PublishAudioVideoMediaUpdatedHandler
+from core.video.application.events.integration_events import AudioVideoMediaUpdatedIntegrationEvent
+from core.video.domain.events.events import AudioVideoMediaUpdated
+
+logger = logging.getLogger(__name__)
 
 class MessageBus(AbstractMessageBus):
     def __init__(self) -> None:
-        self.handlers: dict[Type[Event], list[Handler]] = {}
+        self.handlers: dict[Type[Event], list[Handler]] = {
+            AudioVideoMediaUpdatedIntegrationEvent: [
+                PublishAudioVideoMediaUpdatedHandler(dispatcher=RabbitMQDispatcher(queue="videos.new")),
+            ],
+            AudioVideoMediaUpdated: [
+                DummyHandler(),
+            ],
+        }
     
     def handle(self, events: list[Event]) -> None:
         for event in events:
@@ -14,4 +28,5 @@ class MessageBus(AbstractMessageBus):
                 try:
                     handler.handle(event)
                 except Exception as e:
-                    print(e)
+                    logger.exception("Exception handling event %s", event)
+                    continue
